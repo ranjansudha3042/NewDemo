bf50d066f27a025ea1f64dcd9573708059d12355
Sudhanshu Ranjan <Sudhanshu Ranjan@DESKTOP-BKVN8NT>
2022-02-23 22:17:14 -0800

    First Commit(by Sudha)

---
+++ src/Demo.java
@@ -0,0 +1,36 @@
+
+
+public class Demo {
+
+	public static void main(String[] args) {
+		 
+		                              ////  Int
+	
+		int a= 20;
+		int b= 40;
+		int num=a+b;
+		System.out.println("The sum is = " + num);
+		
+		                             ////  String
+		
+		String sudh= "My FirstName is Sudhanshu";
+		String ranj= "My Surname is Ranjan";
+		System.out.println("The length is = " + sudh.length());
+		System.out.println(sudh.toUpperCase());
+		System.out.println(sudh.toLowerCase());
+		System.out.println(sudh.indexOf("Sudhanshu"));
+		System.out.println(sudh.concat(ranj));
+		
+		                            ////  Math function
+		
+		
+		System.out.println(Math.max(a, b));
+		System.out.println(Math.abs(-10));
+		
+		                           //// Boolean
+		
+		System.out.println(a>b);   
+
+	}
+
+}
